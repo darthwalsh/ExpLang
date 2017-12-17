5043a1f7d6972921870c825c5aea1ed524bc7fb9
Carl Walsh <darthwalsh@gmail.com>
2017-12-17 04:08:39 -0800

    pretty printer
    normalize whitespace for token expression matching

--- MathExp/Printer.cs
+++ MathExp/Printer.cs
@@ -9,6 +9,27 @@ namespace MathExp
 {
     public class Printer
     {
+        public string PrettyPrint(string s, IEnumerable<Token> tokens) {
+            var builder = new StringBuilder();
 
+            var fence = "";
+            foreach (var t in tokens) {
+                builder.Append(fence);
+                builder.Append(s, t.Start, t.Length);
+
+                switch (t.Type) {
+                    case TokenType.CharVar:
+                        fence = "";
+                        break;
+                    default:
+                        fence = " ";
+                        break;
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        public static string Prettify(string s) => new Printer().PrettyPrint(s, new Lexer().Lex(s));
     }
 }
---
+++ MathExpTests/PrinterTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using MathExp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathExpTests
+{
+    [TestClass]
+    public class PrinterTests
+    {
+        [TestMethod]
+        public void Expression() {
+            Verify("", "");
+            Verify(" 12 ", "12");
+            Verify(" 12 + $a ", "12 + $a");
+        }
+
+        [TestMethod]
+        public void Ops() {
+            Verify("+-*/", "+ - * /");
+            Verify("1+2+", "1 + 2 +");
+        }
+
+        [TestMethod]
+        public void Variable() {
+            Verify("$a0", "$a0");
+            Verify("$a0$a1", "$a0 $a1");
+        }
+
+        [TestMethod]
+        public void CharVar() {
+            Verify("_a0", "_a0");
+            Verify("1+_a0+2", "1 + _a0 + 2");
+        }
+
+        static void Verify(string input, string pretty) {
+            var actual = Printer.Prettify(input);
+            Assert.AreEqual(pretty, actual);
+        }
+    }
+}
