f41e9b8e50bfa112dcea769659f9f5501a20e3cd
Carl Walsh <darthwalsh@gmail.com>
2018-12-21 01:17:08 -0800

    web error handling

--- Web/wwwroot/main.js
+++ Web/wwwroot/main.js
@@ -1,21 +1,25 @@
 "use strict";
 
 function $(id) { 
-  return document.getElementById(id);
+    return document.getElementById(id);
 }
 
 window.onload = () => {
-    $("button").onclick = async () => {
-        var request = await fetch("values", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                text: $("input").textContent,
-            })
-        });
-        const result = await request.json();
-        $("output").textContent = result.result;
-    };
+  $("button").onclick = async () => {
+      try {
+          var request = await fetch("values", {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json"
+              },
+              body: JSON.stringify({
+                  text: $("input").textContent,
+              })
+          });
+          const result = await request.json();
+          $("output").textContent = result.result;
+      } catch (ex) {
+          $("output").textContent = `js error: ${ex}`;
+      }
+  };
 };
